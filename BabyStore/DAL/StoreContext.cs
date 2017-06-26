6e1f25925eddee71da1b4e3e8aec8c8b406f3fde
Soma <Soma@SOMA-PC>
2017-06-26 21:39:41 +0200

    Added categories controller.

--- BabyStore/DAL/StoreContext.cs
+++ BabyStore/DAL/StoreContext.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 
 namespace BabyStore.DAL
 {
-    public class StoreContext
+    public class StoreContext:DbContext
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
