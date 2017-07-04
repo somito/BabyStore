e278702a7d84c26351d0153dfec2a0d4bf5cf3b0
hu3soan0 <hu3soan0@HU03ON20138>
2017-07-04 15:39:22 +0200

    added two helper methods and updated the create post method of the product images controller to upload

--- BabyStore/Controllers/ProductImagesController.cs
+++ BabyStore/Controllers/ProductImagesController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using BabyStore.DAL;
 using BabyStore.Models;
+using System.Web.Helpers;
 
 namespace BabyStore.Controllers
 {
@@ -37,7 +38,7 @@ namespace BabyStore.Controllers
         }
 
         // GET: ProductImages/Create
-        public ActionResult Create()
+        public ActionResult Upload()
         {
             return View();
         }
@@ -47,16 +48,40 @@ namespace BabyStore.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,FileName")] ProductImage productImage)
+        public ActionResult Upload(HttpPostedFileBase file)
         {
+            //check if the user has entered a file
+            if (file != null)
+            {
+                //check if the file is valid
+                if (ValidateFile(file))
+                {
+                    try
+                    {
+                        SaveFileToDisk(file);
+                    }
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError("FileName", "Sorry an error occurred saving the file to disk, please try again");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("FileName", "The file must be gif, png, jpeg or jpg and less than 2MB in size");
+                }
+            }
+            else
+            {
+                //if the user has not entered a file return an error message
+                ModelState.AddModelError("FileName", "Please choose a file");
+            }
             if (ModelState.IsValid)
             {
-                db.ProductImages.Add(productImage);
+                db.ProductImages.Add(new ProductImage { FileName = file.FileName });
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(productImage);
+            return View();
         }
 
         // GET: ProductImages/Edit/5
@@ -123,6 +148,33 @@ namespace BabyStore.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private bool ValidateFile(HttpPostedFileBase file)
+        {
+            string fileExtension = System.IO.Path.GetExtension(file.FileName).ToLower();
+            string[] allowedFileTypes = { ".gif", ".png", ".jpeg", ".jpg" };
+            if ((file.ContentLength > 0 && file.ContentLength < 2097152) &&
+            allowedFileTypes.Contains(fileExtension))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void SaveFileToDisk(HttpPostedFileBase file)
+        {
+            WebImage img = new WebImage(file.InputStream);
+            if (img.Width > 190)
+            {
+                img.Resize(190, img.Height);
+            }
+            img.Save(Constants.ProductImagePath + file.FileName);
+            if (img.Width > 100)
+            {
+                img.Resize(100, img.Height);
+            }
+            img.Save(Constants.ProductThumbnailPath + file.FileName);
         }
     }
 }
