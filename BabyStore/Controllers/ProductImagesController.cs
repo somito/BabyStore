c322581d96bfa88f62e2b74bc011068e1e5d3de1
hu3soan0 <hu3soan0@HU03ON20138>
2017-07-10 15:16:50 +0200

    deleted unnecessary links and methods for editing images and details. changed main baby store link to route to productsindex.

--- BabyStore/Controllers/ProductImagesController.cs
+++ BabyStore/Controllers/ProductImagesController.cs
@@ -24,22 +24,7 @@ namespace BabyStore.Controllers
         {
             return View(db.ProductImages.ToList());
         }
-
-        // GET: ProductImages/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProductImage productImage = db.ProductImages.Find(id);
-            if (productImage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(productImage);
-        }
-
+        
         // GET: ProductImages/Create
         public ActionResult Upload()
         {
@@ -153,38 +138,7 @@ namespace BabyStore.Controllers
             }
             return View();
         }
-
-    // GET: ProductImages/Edit/5
-    public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            ProductImage productImage = db.ProductImages.Find(id);
-            if (productImage == null)
-            {
-                return HttpNotFound();
-            }
-            return View(productImage);
-        }
-
-        // POST: ProductImages/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FileName")] ProductImage productImage)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(productImage).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(productImage);
-        }
-
+        
         // GET: ProductImages/Delete/5
         public ActionResult Delete(int? id)
         {
