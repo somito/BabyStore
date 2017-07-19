1c4d425dc8a7e211a4d0377a9e6baa38da5adc6b
hu3soan0 <hu3soan0@HU03ON20138>
2017-07-19 14:21:37 +0200

    Added AdminViewModel, RolesAdminController, and Index view for Roles.

---
+++ BabyStore/Controllers/RolesAdminController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using
+Microsoft.AspNet.Identity.Owin;
+
+namespace BabyStore.Controllers
+{
+    public class RolesAdminController : Controller
+    {
+        public RolesAdminController()
+        {
+        }
+        public RolesAdminController(ApplicationUserManager userManager, ApplicationRoleManager
+        roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ??
+                HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+        private ApplicationRoleManager _roleManager;
+        public ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return _roleManager ?? HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _roleManager = value;
+            }
+        }
+
+        // GET: RolesAdmin
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+
+        // GET: RolesAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RolesAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RolesAdmin/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolesAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RolesAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RolesAdmin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RolesAdmin/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BabyStore/ViewModels/AdminViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BabyStore.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+    }
+}
