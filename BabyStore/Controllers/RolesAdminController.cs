50239280efff95bdf940b6b762a11cb9392d0e71
hu3soan0 <hu3soan0@HU03ON20138>
2017-07-20 15:50:24 +0200

    Updated Details method in rolesAdminController and created view to display users for role.

--- BabyStore/Controllers/RolesAdminController.cs
+++ BabyStore/Controllers/RolesAdminController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using
-Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using System.Net;
+using System.Threading.Tasks;
+using BabyStore.Models;
 
 namespace BabyStore.Controllers
 {
@@ -53,9 +55,25 @@ namespace BabyStore.Controllers
         }
 
         // GET: RolesAdmin/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(string id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            // Get the list of Users in this Role
+            var users = new List<ApplicationUser>();
+            foreach (var user in UserManager.Users.ToList())
+            {
+                if (await UserManager.IsInRoleAsync(user.Id, role.Name))
+                {
+                    users.Add(user);
+                }
+            }
+            ViewBag.Users = users;
+            ViewBag.UserCount = users.Count();
+            return View(role);
         }
 
         // GET: RolesAdmin/Create
