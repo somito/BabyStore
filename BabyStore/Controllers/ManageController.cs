583547dbc9a738b8d1cb59a5da91cea2d497cef6
hu3soan0 <hu3soan0@HU03ON20138>
2017-08-14 15:52:53 +0200

    added the controller method and view to manage controller to show user details when clicking on the user name. also created a partial view for this with showing user details from usersadmin controller

--- BabyStore/Controllers/ManageController.cs
+++ BabyStore/Controllers/ManageController.cs
@@ -52,27 +52,11 @@ namespace BabyStore.Controllers
 
         //
         // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
+        public async Task<ActionResult> Index()
         {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
-                : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
-                : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
-                : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
-                : "";
-
             var userId = User.Identity.GetUserId();
-            var model = new IndexViewModel
-            {
-                HasPassword = HasPassword(),
-                PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
-                TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
-                Logins = await UserManager.GetLoginsAsync(userId),
-                BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId)
-            };
-            return View(model);
+            var user = await UserManager.FindByIdAsync(userId);
+            return View(user);
         }
 
         //
