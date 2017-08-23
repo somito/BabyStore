799b4070e92b2b51ec8c70d853386e1da2166bef
hu3soan0 <hu3soan0@HU03ON20138>
2017-08-23 15:33:25 +0200

    Added basketviewmodel and basketcontroller with a few methods.

---
+++ BabyStore/Controllers/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using BabyStore.Models;
+using BabyStore.ViewModels;
+using System.Web.Mvc;
+
+namespace BabyStore.Controllers
+{
+    public class BasketController : Controller
+    {
+        // GET: Basket
+        public ActionResult Index()
+        {
+            Basket basket = Basket.GetBasket();
+            BasketViewModel viewModel = new BasketViewModel
+            {
+                BasketLines = basket.GetBasketLines(),
+                TotalCost = basket.GetTotalCost()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddToBasket(int id, int quantity)
+        {
+            Basket basket = Basket.GetBasket();
+            basket.AddToBasket(id, quantity);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateBasket(BasketViewModel viewModel)
+        {
+            Basket basket = Basket.GetBasket();
+            basket.UpdateBasket(viewModel.BasketLines);
+            return RedirectToAction("Index");
+        }
+    }
+}
