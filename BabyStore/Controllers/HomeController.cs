114b403da0c7b89566d664f8953b4c3b436716db
Soma <Soma@SOMA-PC>
2017-09-13 21:26:24 +0200

    added asynchronous access to first 4 best sellers and presented them in the home index view.

--- BabyStore/Controllers/HomeController.cs
+++ BabyStore/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BabyStore.DAL;
+using BabyStore.ViewModels;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace BabyStore.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private StoreContext db = new StoreContext();
+
+        public async Task<ActionResult> Index()
         {
-            return RedirectToAction("Index", "Products");
+            var topSellers = (from topProducts in db.OrderLines
+                              where (topProducts.ProductID != null)
+                              group topProducts by topProducts.Product into topGroup
+                              select new BestSellersViewModel
+                              {
+                                  Product = topGroup.Key,
+                                  SalesCount = topGroup.Sum(o => o.Quantity),
+                                  ProductImage = topGroup.Key.ProductImageMappings.
+                              OrderBy(pim => pim.ImageNumber).FirstOrDefault().ProductImage.FileName
+                              }).OrderByDescending(tg => tg.SalesCount).Take(4);
+            return View(await topSellers.ToListAsync());
         }
 
         public ActionResult About(string id)
